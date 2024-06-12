16fcbb6dd15f62962b101c1032aab2dd5ac9d8e0
sweetSD <an060875@naver.com>
2024-06-13 01:07:50 +0900

    Sphere 추가

--- Assets/AnimationEventTest.cs
+++ Assets/AnimationEventTest.cs
@@ -3,16 +3,20 @@ using UnityEngine;
 public class AnimationEventTest : MonoBehaviour
 {
     [SerializeField] private Animation.AnimationEvent animationEvent;
+    [SerializeField] private GameObject attackCollider;
     
-    void Start()
+    void Awake()
     {
         animationEvent["MotionStarted"] += _ => Debug.Log($"Motion Started.");
         animationEvent["AttackStarted"] += @event => Debug.Log($"Attack Started with Int Param: {@event.intParameter}.");
+        animationEvent["AttackStarted"] += _ => attackCollider.gameObject.SetActive(true);
         animationEvent["AttackEnded"] += OnAttackEnded;
     }
 
     private void OnAttackEnded(AnimationEvent @event)
     {
         Debug.Log($"Attack Ended with Float Param: {@event.floatParameter}.");
+        attackCollider.gameObject.SetActive(false);
     }
 }
+
